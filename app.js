3577ed32966cad03500cdd39ddbca53f2a12d2c0
Yash Lalit <yashlalit.23@gmail.com>
2021-05-04 10:50:10 +0530

    Code refractored & Upgrade to Firesbase Admin SDK.

--- app.js
+++ app.js
@@ -2,19 +2,12 @@
 const worker = require('./script'); //--> Importing the script for using the worker.
 const cron = require('node-cron'); //--> Using node-cron for scheduling the executions.
 
-// //Scheduling worker to fetch every 6 hours articles/data
-// cron.schedule("0 0 */6 * * *",async ()=>{
-//     await worker.execute(false);    
-// });
+//Scheduling worker to fetch every 6 hours articles/data
+cron.schedule("0 0 */6 * * *",async ()=>{
+    await worker.execute(false);    
+});
 
-// ///Scheduling worker to fetch every 2 hours articles/data
-// cron.schedule("0 0 */2 * * *",async ()=>{
-//     await worker.execute(true);    
-// });
-
-async function start(){
-    await worker.execute(false);  
-    await worker.execute(true);  
-}
-
-start();
+///Scheduling worker to fetch every 2 hours articles/data
+cron.schedule("0 0 */2 * * *",async ()=>{
+    await worker.execute(true);    
+});
